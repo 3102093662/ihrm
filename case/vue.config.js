4457cca694972914c529f49a27133b87ff7448ce
3102093662 <3102093662@qq.com>
2023-07-04 20:44:07 +0800

    测试

--- case/vue.config.js
+++ case/vue.config.js
@@ -1,5 +1,24 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: "./",
+  outputDir: "dist",
+  assetsDir: "./assets",
+  devServer: {
+    port: 7266,
+    host: "0.0.0.0",
+    https: false,
+    open: false,
+    proxy: {
+      "/api": {
+        changeOrigin: true,
+        secure: false,
+        target: "https://wwww.baidu.com",
+        pathRewrite: {
+          "^.api": ""
+        }
+      }
+    }
+  }
 })
