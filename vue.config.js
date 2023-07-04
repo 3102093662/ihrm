87d7f7d1f040cd6c1acbb5080252d6ffb8868364
3102093662 <3102093662@qq.com>
2023-07-04 21:39:10 +0800

    fix(a): a
    a

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,23 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
 	transpileDependencies: true,
 	lintOnSave: false,
+	publicPath: "./",
+	assetsDir: "assets",
+	outputDir: "dist",
+	devServer: {
+		port: 7666,
+		host: "0.0.0.0",
+		https: true,
+		open: false,
+		proxy: {
+			"/api": {
+				changeOrigin: true,
+				secure: false,
+				target: "http://www.google.com",
+				pathRewrites: {
+					"^/api": "",
+				},
+			},
+		},
+	},
 });
