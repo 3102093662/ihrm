af8098956d25aede6ff678482235de74b3b3e3b0
3102093662 <3102093662@qq.com>
2023-07-04 20:56:30 +0800

    完成

--- case/src/main.js
+++ case/src/main.js
@@ -2,7 +2,12 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
+import ElementUI from "element-ui"
+import "element-ui/lib/theme-chalk/index.css"
+import httpRequest from "@/utils/httpRequest"
 
+Vue.prototype.$http = httpRequest
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
